ca81f0c2660834d1f25a7fd3af3f9a2d72965c30
BrognietGeoffrey <geoffrey.brogniet@gmail.com>
2021-03-30 19:26:31 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -20,7 +20,7 @@ export default new Vuex.Store({
     },
     endpoints: {
       login: "http://localhost:3000/login",
-      products: "http://localhost:8080/products"
+      products: "http://localhost:8000/products"
     },
    },
    getters: {
