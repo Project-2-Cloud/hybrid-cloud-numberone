bee1915c67fd2bf24a26df6b58e5d89846a50acc
Jerome <jerome.demeester@student.ucll.be>
2021-03-31 14:09:23 +0200

    fixed CORS

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import createPersistedState from "vuex-persistedstate";
 Vue.use(Vuex)
 
 var url;
-const headers = { Accept: "application/json", "Access-Control-Allow-Origin":"http://hybrid-cloud-numberone-web-ucllteam01.ocp-ucll-40cb0df2b03969eabb3fac6e80373775-0000.eu-de.containers.appdomain.cloud/api/product/productsOverview" };
+const headers = { Accept: "application/json" };
 
 export default new Vuex.Store({
   plugins: [createPersistedState()],
